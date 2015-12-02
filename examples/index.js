7a104a04caaba443773419ac9b86b6c5f5b6d372
Xavier O'Neil <xavier@openfin.co>
2015-12-02 08:52:43 -0500

    Update index.js

--- examples/index.js
+++ examples/index.js
@@ -5,13 +5,18 @@ var label = document.getElementById('label'),
     ulist = document.getElementById('ulist');
 
 /*
+    create an ocean
+ */
+var ocn = ocean();
+
+/*
     Set up your shared state.
 */
-var counter = ocean.update(() => {
+var counter = ocn.update(() => {
     return 0;
 });
 
-var users = ocean.update(() => {
+var users = ocn.update(() => {
     return [{
         name: 'bob',
         occupation: 'builder'
@@ -25,52 +30,52 @@ var users = ocean.update(() => {
 console.log('coutner: "%s" users: "%s" ', counter, users);
 
 //first pass paint 
-paintUsers(ocean.getItem(users));
+paintUsers(ocn.getItem(users));
 
 
 /*
     Wire up events
 */
 btn1.addEventListener('click', function() {
-    ocean.dispatch('btn1_clicked');
+    ocn.dispatch('btn1_clicked');
 });
 
 btn2.addEventListener('click', () => {
-    ocean.dispatch('btn2_clicked');
+    ocn.dispatch('btn2_clicked');
 });
 
 btn3.addEventListener('click', () => {
-    ocean.dispatch('btn3 clicked');
+    ocn.dispatch('btn3 clicked');
 });
 
 
 /* bool state functions... */
 var coutnerGt5 = () => {
-    return ocean.getItem(counter) > 5;
+    return ocn.getItem(counter) > 5;
 }
 
 var coutnerLt5 = () => {
-    return ocean.getItem(counter) < 5;
+    return ocn.getItem(counter) < 5;
 }
 
 
 /*
     React to events
 */
-var unsub1 = ocean.subscribe('btn1_clicked', () => {
+var unsub1 = ocn.subscribe('btn1_clicked', () => {
     //increment counter 
-    ocean.update(counter, (crt) => {
+    ocn.update(counter, (crt) => {
         return ++crt;
     });
 });
 
-ocean.changed(counter, () => {
-    label.innerHTML = ocean.getItem(counter);
+ocn.changed(counter, () => {
+    label.innerHTML = ocn.getItem(counter);
 });
 
-ocean.subscribe('btn2_clicked', () => {
+ocn.subscribe('btn2_clicked', () => {
     // add users to a list
-    ocean.update(users, (usrs) => {
+    ocn.update(users, (usrs) => {
         usrs.push({
             name: 'robby',
             occupation: 'robber'
@@ -80,23 +85,23 @@ ocean.subscribe('btn2_clicked', () => {
     });
 });
 
-ocean.changed(users, (usrs) => {
+ocn.changed(users, (usrs) => {
     paintUsers(usrs);
 });
 
-ocean.compsub('btn1_clicked and btn2_clicked', () => {
+ocn.compsub('btn1_clicked and btn2_clicked', () => {
     console.log('holy shit');
 });
 
-ocean.compsub('btn1_clicked and btn2_clicked or btn3 clicked', () => {
+ocn.compsub('btn1_clicked and btn2_clicked or btn3 clicked', () => {
     console.log('holy shit, a 3ed case');
 })
 
-ocean.compsub(['btn1_clicked and $', coutnerGt5], () => {
+ocn.compsub(['btn1_clicked and $', coutnerGt5], () => {
     console.log('woooooow');
 });
 
-ocean.compsub(['btn1_clicked and $ or btn2_clicked and $', coutnerGt5, coutnerLt5], () => {
+ocn.compsub(['btn1_clicked and $ or btn2_clicked and $', coutnerGt5, coutnerLt5], () => {
     console.log('tricky tricky...');
 });
 
